adfc5f78ae57ac253dfb5bd660b04da68216d4b8
snoopdesigns <snoopdesigns@gmail.com>
2015-01-24 22:07:36 +0300

    1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,6 +22,7 @@ public class Main extends HttpServlet {
   private void showHome(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
     resp.getWriter().print("Hello from Java!");
+    System.out.println("This is test log");
   }
 
   private void showDatabase(HttpServletRequest req, HttpServletResponse resp)
