6d07a20c6eeab00bea0554f5b402fa3c407c25d4
snoopdesigns <snoopdesigns@gmail.com>
2015-04-23 00:44:39 +0300

    Added gallery viewer and contacts page

--- src/main/resources/webapp/js/script.js
+++ src/main/resources/webapp/js/script.js
@@ -1,6 +1,7 @@
 window.onload = function() {
     onclickabout();
     setTimeout(function() {document.getElementById('about-block').style.transition = "All 1s ease";}, 2000);
+    initLightbox();
 }
 
 var prevHeight = 0;
@@ -67,6 +68,19 @@ function drawGraphs(div) {
             var myDoughnutChart2 = new Chart(document.getElementById("myChart2").getContext("2d")).Doughnut(graph2Data,options);
             var myDoughnutChart3 = new Chart(document.getElementById("myChart3").getContext("2d")).Doughnut(graph3Data,options);
         }
+
+        if(div.scrollTop > 700 && prevHeight <= 700) {
+            document.getElementById('first_img').style.opacity = "1.0";
+            setTimeout(function() {document.getElementById('second_img').style.opacity = "1.0";}, 200);
+            setTimeout(function() {document.getElementById('third_img').style.opacity = "1.0";}, 400);
+        }
+
+        if(div.scrollTop <200 && prevHeight >= 200) {
+            document.getElementById('first_img').style.opacity = "0.0";
+            document.getElementById('second_img').style.opacity = "0.0";
+            document.getElementById('third_img').style.opacity = "0.0";
+        }
+
         prevHeight = div.scrollTop;
 }
 
@@ -100,7 +114,6 @@ function onclickabout() {
         document.getElementById('about-block').style.overflow = "auto";
         document.getElementById('about-block').style.overflowY = "auto";
         document.getElementById('about-block').style.height = computeWindowHeight() + "px";
-        computeWindowHeight();
         setTimeout(function() {document.getElementById('about-block').style.opacity = "1";}, 100); }
     , 1000);
 }
@@ -112,7 +125,7 @@ function computeWindowHeight() {
         g = d.getElementsByTagName('body')[0],
         x = w.innerWidth || e.clientWidth || g.clientWidth,
         y = w.innerHeight|| e.clientHeight|| g.clientHeight;
-    return (y - 290);
+    return (y - 160);
 }
 
 function onclickexample() {
@@ -126,7 +139,9 @@ function onclickexample() {
         document.getElementById('contact-block').style.display = "none";
         document.getElementById('about-block').style.display = "none";
         document.getElementById('example-block').style.display = "block";
-        document.getElementById('example-block').style.overflow = "visible";
+        document.getElementById('example-block').style.overflow = "auto";
+        document.getElementById('example-block').style.overflowY = "auto";
+        document.getElementById('example-block').style.height = computeWindowHeight() + "px";
         setTimeout(function() {document.getElementById('example-block').style.opacity = "1";}, 100); }
     , 1000);
 }
